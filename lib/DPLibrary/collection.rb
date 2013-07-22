c9d5d52f2a381998dcf7b72360589fd04f0f9eef
Chad Fennell <fenne035@umn.edu>
2013-07-22 16:40:59 -0500

    Collections are sometimes an array; break collections down into collections and collection items

--- lib/DPLibrary/collection.rb
+++ lib/DPLibrary/collection.rb
@@ -1,17 +1,23 @@
 module DPLibrary
   class Collection
-    attr_accessor :id,
-                  :title,
-                  :name
+    attr_accessor :collection_items
 
-    def initialize(hash)
-      set_values(hash)
+    def initialize(collection_response)
+      set_values(collection_response)
     end
 
-    def set_values(collection_hash)
-      self.id = collection_hash['id']
-      self.title = collection_hash['title']
-      self.name = collection_hash['name']
+    def set_values(collection_response)
+      @collection_items = []
+      if (collection_response.is_a?(Array))
+        @collection_items = collection_response.map {|h| create_collection_item(h)}
+      else
+        @collection_items << CollectionItem.new(collection_response)
+      end
+      @collection_items
+    end
+
+    def create_collection_item(collection_item_hash)
+      CollectionItem.new(collection_item_hash)
     end
   end
 end
